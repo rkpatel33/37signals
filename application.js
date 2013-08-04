0c1139a22cbaae7825d94fec78cc8aae932cf674
rkpatel33 <mail@rishinyc.com>
2013-08-04 14:44:58 -0400

    Finished.  Arrows not positioned properly.

--- application.js
+++ application.js
@@ -1,16 +1,47 @@
 
 $(document).ready(function(){
 
-	$('.bc').on('mouseenter', function() {
-		$('.header').hide();
-		$('.hidden bc').show();
+	$('.products .bc').on('mouseenter', function() {
+		$('header').hide();
+		$(this).closest('body').find('.bc.banner').removeClass("hidden");
+		$(this).closest('body').find('.bc.banner').addClass("banner");
 	});
 
-	$('.bc').on('mouseleave', function() {
-		$('.header').show();
-		$('.hidden bc').hide();
+	$('.products .bc').on('mouseleave', function() {
+		$('header').show();
+		$(this).closest('body').find('.bc.banner').addClass("hidden");
+		$(this).closest('body').find('.bc.banner').addClass("hidden");
 	});
 	
+
+
+
+	$('.products .hr').on('mouseenter', function() {
+		$('header').hide();
+		$(this).closest('body').find('.hr.banner').removeClass("hidden");
+		$(this).closest('body').find('.hr.banner').addClass("banner");
+	});
+
+	$('.products .hr').on('mouseleave', function() {
+		$('header').show();
+		$(this).closest('body').find('.hr.banner').addClass("hidden");
+		$(this).closest('body').find('.hr.banner').addClass("hidden");
+	});
+
+
+
+	$('.products .cf').on('mouseenter', function() {
+		$('header').hide();
+		$(this).closest('body').find('.cf.banner').removeClass("hidden");
+		$(this).closest('body').find('.cf.banner').addClass("banner");
+	});
+
+	$('.products .cf').on('mouseleave', function() {
+		$('header').show();
+		$(this).closest('body').find('.cf.banner').addClass("hidden");
+		$(this).closest('body').find('.cf.banner').addClass("hidden");
+	});
+
 
 });
 
